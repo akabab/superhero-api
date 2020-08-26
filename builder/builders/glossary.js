96d65dc2ca7c52fd5c5709e02d20eaa0c3192533
Yoann Cribier <yoann_cribier@hotmail.com>
2020-08-26 19:45:53 +0400

    replaced rawcdn.githack.com -> cdn.jsdelivr.net from cdn urls (faster CDN & more reliable)

--- builder/builders/glossary.js
+++ builder/builders/glossary.js
@@ -1,7 +1,7 @@
 const { version } = require('../../package.json')
 
 const baseUrl = `https://akabab.github.io/superhero-api/api`
-const cdnBaseUrl = `https://rawcdn.githack.com/akabab/superhero-api/${version}/api`
+const cdnBaseUrl = `https://cdn.jsdelivr.net/gh/akabab/superhero-api/${version}/api`
 
 const glossary = heroes => `# Superhero Glossary
 
--- builder/index.js
+++ builder/index.js
@@ -5,7 +5,7 @@ const { promisify } = require('util')
 const { version } = require('../package.json')
 
 const baseUrl = `https://akabab.github.io/superhero-api/api`
-const cdnBaseUrl = `https://rawcdn.githack.com/akabab/superhero-api/${version}/api`
+const cdnBaseUrl = `https://cdn.jsdelivr.net/gh/akabab/superhero-api/${version}/api`
 
 const { buildImages } = require('./builders/images')
 const { documentation } = require('./builders/documentation')
